1c1ddb1e2fb278702aa0e86f6484c04c4a8518f4
eramarokoto <elisoa.ramarokoto@insa-rouen.fr>
2015-02-15 17:51:27 +0100

    MAJ générale

--- BeaconSandwich/www/js/app.js
+++ BeaconSandwich/www/js/app.js
@@ -10,46 +10,10 @@
     };
   });
 
-  module.controller('DetailController', function($scope, $data) {
-    $scope.item = $data.selectedItem;
-  });
-
-  module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
-    };
-  });
-
+  // possibilité de bouchon créé via le JS
   module.factory('$data', function() {
       var data = {};
-      
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          }
-      ]; 
-      
+      data.items = []; 
       return data;
   });
 })();
