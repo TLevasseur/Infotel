32bd6c2261dae0bd4035f7741b51d669e0c1a40e
TLevasseur <levasseur.thibaud@gmail.com>
2015-02-17 21:34:36 +0100

    Putin de commit de ouf malade de la mort qui tue

--- BeaconSandwich/www/js/app.js
+++ BeaconSandwich/www/js/app.js
@@ -25,65 +25,7 @@
   });
 
   module.factory('$data', function() {
-    var data = {};
-    
-    data.LISTEDEFLUX = [
-    
-    
-    {
-      NOM:'Les annonces du BDE',
-      LISTEDEMESSAGES:
-      {
-        MESSAGE1:
-        {
-          TITRE:'Construction d une fontaine de chocolat',
-          DATE:'09/09/9999',
-          SOURCE:'Pole communication',
-          INFOS:'          Bonjour          Suite à la recette exceptionnelle de cette année, nous allons construire une fontaine de chocolat en septembre.'
-        },
-        MESSAGE2:
-        {
-          TITRE:'Faillite ',
-          DATE:'03/03/2015',
-          SOURCE:'Direction',
-          INFOS:'Bonjour          Le BDE est à la rue depuis un mois.'
-        }
-
-        
-      }
-      
-    },
-    {
-      NOM:'Menu de la Cantine',
-      LISTEDEMESSAGES:
-      {
-        MESSAGE1:
-        {
-          TITRE:'Menu de demain',
-          DATE:'07/02/2015',
-          SOURCE:'Monique de la cuisine',
-          INFOS:'Bonjour          Ce mardi, nous aurons pour déjeuner du poulet aux olives.'
-        },
-        MESSAGE2:
-        {
-          TITRE:'Menu de ce midi ',
-          DATE:'02/03/2015',
-          SOURCE:'Monique de la cuisine',
-          INFOS:'          Bonjour          Ce lundi, nous aurons pour déjeuner du boudain noir.'
-        }
-
-        
-      }
-      
-    }
-
-    
-
-    
-    ]; 
-
-      // data=fonctionQuiREnvoieTableau()
-      
+    var data = dataDuFabuleuxMondeDInternet();   
       return data;
     });
 })();
@@ -99,3 +41,277 @@
 //          }
 //        }
 //      </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//
+//            Partie de Thibaud ne pas toucher 
+//
+//
+//
+//
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//stockage dégeu parce que fuck you
+//Merci de ne pas me les piquer et de bien changer vos noms à vous !
+var DATAS;
+var AjaxCaching = false;
+
+
+
+//fonction pricipales
+//A appeller dès qu'on veux refresh les datas !
+function dataDuFabuleuxMondeDInternet(){
+    DATAS=null;
+    var xhr = createXHR();
+  
+    var script = "http://levasseur.tf/beaconsandwich/flux.php";
+    var filename = "http://levasseur.tf/beaconsandwich/flux.xml";   
+           
+    xhr.onreadystatechange=function()
+    { 
+        if(xhr.readyState == 4)
+        {
+             retrieve(filename);
+        }
+    }; 
+        xhr.open("POST", script, false);//requette en asychrone dépréciated wesh     
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(null);
+
+        return DATAS;
+
+}
+
+
+
+
+//Code pour récupérer le petit fichier XML sur le serveur.
+//Fonction qui crée des requettes propres quand on a besoin !
+function createXHR() {
+    var xhrObj;
+    
+    if (window.XMLHttpRequest) {
+        // branch for native XMLHttpRequest object - Mozilla, IE7
+        try {
+            xhrObj = new XMLHttpRequest({mozSystem: true});
+        } catch (e) {
+            xhrObj = null;
+        }
+    } else if (window.createRequest) {
+        /* For ICEbrowser -- untested.
+         * per their site 
+         * http://support.icesoft.com/jive/entry.jspa?entryID=471&categoryID=21
+         */
+        try {
+            xhrObj = window.createRequest();
+        }
+        catch (e) {
+            xhrObj = null;
+        }
+    } else if (window.ActiveXObject) {
+        // branch for IE/Windows ActiveX version
+        try {
+            xhrObj = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch(e) {
+            try{
+                xhrObj = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch(e) {
+                xhrObj = null;
+            }
+        }//catch
+    } //if-else
+    return xhrObj;
+}
+
+
+
+//fonction qui fais des chauses qui nous interesse bien
+function retrieve(url){
+    var storage = document.getElementById("storage");
+    var xhr = createXHR();
+    xhr.onreadystatechange=function()
+    { 
+        if(xhr.readyState == 4)
+        {
+            if(xhr.status == 200)
+            {
+                var     content = xhr.responseXML;
+                var lesFlux=dataFromXML(content);
+                DATAS=lesFlux;
+                //descriptionConsole(lesFlux);
+                //display(lesFlux, storage);
+            }
+        }
+    }; 
+
+    if(AjaxCaching == false)
+        url = url + "?nocache=" + Math.random();
+    xhr.open("GET", url , false);
+    xhr.send(null); 
+}
+
+
+
+
+
+
+
+//Le code d'interprétation de ce qu'on a obtenu avec les méthodes du dessus
+//création des différents objets et instanciation à la volée
+
+
+
+
+
+
+//Méthodes constructeur pour la sérialisation XML vers objet
+function listeFlux(){
+    return {LISTEDEFLUX:new Array(),
+            ajouterflux:function(flux){this.LISTEDEFLUX.push(flux);}
+            };
+}
+
+
+function flux(nom){
+    return {NOM:nom, 
+            LISTEDEMESSAGES:new Array(),
+            ajouterMessage:function(message){this.LISTEDEMESSAGES.push(message);}
+            };
+}
+
+
+function message(source, titre, date, uid, infos){
+    return {TITRE:titre, DATE:date, SOURCE:source, UID:uid, INFOS:infos};
+}
+
+
+//Grosse méthode d'interprétation du fichier XML. bonne traduction ;)
+function dataFromXML(xml){
+    var laListeDesFlux=listeFlux();
+    var lesFlux = xml.getElementsByTagName("flux");
+
+    for (var i = 0; i < lesFlux.length; i++) {
+        var unFlux=flux(lesFlux[i].getElementsByTagName("nom").item(0).firstChild.data);
+
+        var laListeDesMessages=lesFlux[i].getElementsByTagName("message");
+
+        for (var j = 0; j < laListeDesMessages.length; j++) {
+
+            var source=laListeDesMessages[j].getElementsByTagName("source").item(0).firstChild.data;
+            var titre=laListeDesMessages[j].getElementsByTagName("titre").item(0).firstChild.data;
+            var date=laListeDesMessages[j].getElementsByTagName("date").item(0).firstChild.data;
+            var uid=laListeDesMessages[j].getElementsByTagName("uid").item(0).firstChild.data;
+            var infos=laListeDesMessages[j].getElementsByTagName("infos").item(0).firstChild.data;
+
+
+            unFlux.ajouterMessage(message(source,titre,date,uid,infos));
+        };
+        laListeDesFlux.ajouterflux(unFlux);
+    };
+    return laListeDesFlux;
+}
+
+
+//fichier description sous console de ce qu'on obtient
+//genre gros tests
+function descriptionConsole(obj){
+    console.log(obj);
+    console.log(obj.LISTEDEFLUX[0]);
+    console.log(obj.LISTEDEFLUX[0].LISTEDEMESSAGES[0]);
+    console.log(obj.LISTEDEFLUX[0].LISTEDEMESSAGES[1]);
+    console.log(obj.LISTEDEFLUX[1]);
+    console.log(obj.LISTEDEFLUX[1].LISTEDEMESSAGES[0]);
+    console.log(obj.LISTEDEFLUX[1].LISTEDEMESSAGES[1]);
+    console.log(obj.LISTEDEFLUX[2]);
+    console.log(obj.LISTEDEFLUX[2].LISTEDEMESSAGES[0]);
+
+}
+
+//fonction qui bug je sais pas trop pourquoi maintenant !
+function display(content, storage){
+    //storage.innerHTML= content.getElementsByTagName("source").item(0).firstChild.data;
+    storage.innerHTML= content.LISTEDEFLUX[0].LISTEDEMESSAGES[0].INFOS;
+    console.log("lol");
+    console.log(content.LISTEDEFLUX[0].LISTEDEMESSAGES[0].INFOS);
+}
+
